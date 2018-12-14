17db47de70e0a02cef1eada79ba5d80992bbf27d
Pritimay Sarkar <prisar@Pritimays-MacBook-Air.local>
2018-12-14 11:53:51 +0530

    Drag and Drop

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientApp';
+
+  items = ['First', 'Second', 'Third', 'Fourth'];
+ 
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
