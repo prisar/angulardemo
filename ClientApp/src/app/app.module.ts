a3d465234e7765e8cabff0f77686d7abeb83c887
Pritimay Sarkar <prisar@Pritimays-MacBook-Air.local>
2018-12-14 11:47:08 +0530

    Upgrade angular 5 to anular 7

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,50 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TestComponent } from './test/test.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { MoviesComponent } from './components/movies/movies.component';
-import { MovieService } from './shared/services/movie.service';
-import { EditMovieComponent } from './components/editmovie/editmovie.component';
-import { AddmovieComponent } from './components/addmovie/addmovie.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    TestComponent,
-    EmployeeListComponent,
-    MoviesComponent,
-    EditMovieComponent,
-    AddmovieComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'employees', component: EmployeeListComponent },
-      { path: 'movies', component: MoviesComponent },
-      { path: 'editmovie/:id', component: EditMovieComponent },
-      { path: 'addmovie', component: AddmovieComponent },
-    ])
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [MovieService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ README.md
@@ -0,0 +1,20 @@
+
+1. Add material
+
+    npm install --save @angular/material @angular/cdk @angular/animations
+
+2. Add model
+
+    ng g class models/employee --type=model
+
+3. Update angular
+
+    ng update @angular/cli @angular/core
+
+4. The above set didnt work well. So, renamed old ClientApp to ClientApp5 and created a new ClientApp using angular-cli which is the latest. I used routing option and css option.
+
+    ng new ClientApp
+
+5. Installed Material Design
+
+    npm install --save @angular/material @angular/cdk @angular/animations
